88121e949b08d434264d1a7e4a1ca9da3bef0404
aracwong <aracwong@163.com>
2020-04-12 11:01:20 +0800

    个人中心

--- jdshop/pages/login/index.js
+++ jdshop/pages/login/index.js
@@ -9,6 +9,17 @@ Page({
   },
 
   /**
+   * 处理用户信息
+   * @param {*} e 
+   */
+  handleGetUserInfo: function(e){
+    const {userInfo} = e.detail;
+    wx.setStorageSync('userinfo', userInfo);
+    wx.navigateBack({
+      delta: 1
+    })
+  },
+  /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
